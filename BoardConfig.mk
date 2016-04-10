dad4c77a00e7734008ab80e6b9003e2213afa3f2
swapnilxd <swapnilxd@gmail.com>
2016-04-11 04:19:16 +0530

    Revert "kenzo : add mkbootimg"-use qcom/common
    This reverts commit ca599335f2cdea1679c3bd32cbf16972ead74689.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,11 +47,11 @@ TARGET_NO_BOOTLOADER := true
 TARGET_OTA_ASSERT_DEVICE := kenzo
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=disabled
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
+BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x02000000
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8956
